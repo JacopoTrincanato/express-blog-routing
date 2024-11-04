2c71e98dcab31f3e8d22534d3f219f2e5554ac9e
JacopoTrincanato <trincanatoj@gmail.com>
2024-11-04 16:11:31 +0100

    create the server in app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+//creo una variabile che richiama express
+const express = require('express')
+const app = express()
+
+//richiamo la variabile d'ambiente HOST dal file .env
+const HOST = process.env.HOST
+
+//richiamo la variabile d'ambiente PORT dal file .env
+const PORT = process.env.PORT
+
+//starto il server
+app.listen(PORT, (req, res)=>{
+    console.log(`Server is running at ${HOST}:${PORT}`);
+    
+})
