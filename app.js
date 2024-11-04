37047047e7415595e76dbce58e34bcbf5bd558f3
JacopoTrincanato <trincanatoj@gmail.com>
2024-11-04 17:15:35 +0100

    add middleware to app.js

--- app.js
+++ app.js
@@ -2,11 +2,17 @@
 const express = require('express')
 const app = express()
 
+//importo il contenuto di posts.js
+const postRouter = require('./routers/posts.js')
+
 //richiamo la variabile d'ambiente HOST dal file .env
 const HOST = process.env.HOST
 
 //richiamo la variabile d'ambiente PORT dal file .env
 const PORT = process.env.PORT
+
+//gestisco i file statici
+app.use(express.static('/public'));
 
 //starto il server
 app.listen(PORT, (req, res)=>{
@@ -14,7 +20,13 @@ app.listen(PORT, (req, res)=>{
     
 })
 
+//inserisco il middleware
+app.use(express.json())
+
 //creo la prima rotta
 app.get('/', (req, res)=>{
-    res.send('Post rest API')
+    res.send('Post rest API');
 })
+
+//Post API
+app.use('/posts', postRouter);
