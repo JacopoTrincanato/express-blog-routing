7c8c1836c08da47f7d363f218d13f6d91deb793b
JacopoTrincanato <trincanatoj@gmail.com>
2024-11-04 16:30:40 +0100

    try to fix index function

--- controllers/dolcicontroller.js
+++ controllers/dolcicontroller.js
@@ -4,28 +4,25 @@ const posts = require('../db/db.js')
 const fs = require('fs')
 
 const index = (req, res)=>{
-    //prendo il div con la classe container dalla DOM
-    const container = document.querySelector('container')
+
     //cero una ul
-    const postList = document.createElement('ul');
+    let ul = `<ul></ul>`
     posts.forEach(post=>{
 
-        //creo gli li
-        const listItem = document.createElement('li');
-        //inserisco il markup negli li
-        listItem.innerHTML = `
-            <h2>${post.title}</h2>
-            <p>${post.content}</p>
-            <img src="${post.image}" alt="${post.title}">
-            <p><strong>Tags:</strong> ${post.tags.join(', ')}</p>
-        `;
         //inserisco gli li nell'ul
-        postList.appendChild(listItem);
+        ul += `
+            <li>
+                <h2>${post.title}</h2>
+                <p>${post.content}</p>
+                <img src="${post.image}" alt="${post.title}">
+                <p><strong>Tags:</strong> ${post.tags.join(', ')}</p>
+            </li>
+        `;
+        
         
     })
 
-    //inserisco l'ul nel container
-    container.appendChild(postList);
-
-    res.status(200)
+    res.status(200).send(ul)
 }
+
+module.exports = { index }
